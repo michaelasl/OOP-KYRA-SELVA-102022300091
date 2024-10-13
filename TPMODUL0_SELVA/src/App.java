7cfc187abd6ba13715138031b6564d0ce9abeaf2
brigita selva <brigitaselva@MacBook-Pro-brigita.local>
2024-10-13 17:38:54 +0700

    init: modul 0 commit

--- TPMODUL0_SELVA/src/App.java
+++ TPMODUL0_SELVA/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Good Bye, World!");
+        System.out.println("Hello, World!");
     }
 }
